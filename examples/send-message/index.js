28e64d0d5b378ab7fe69d3ea0bd40fddc2aafe33
ParkSB <parkgds@gmail.com>
2020-01-04 16:52:52 +0900

    example: send-message 예제 업데이트

--- examples/send-message/index.js
+++ examples/send-message/index.js
@@ -7,26 +7,28 @@ const thecamp = require('the-camp-lib');
   const id = process.env.USER_ID || '';
   const password = process.env.USER_PWD || '';
 
-  const traineeName = process.env.TRAINEE_NAME || '';
-  const unitName = process.env.UNIT_NAME || '';
+  const name = process.env.TRAINEE_NAME || '';
+  const birth = process.env.TRAINEE_BIRTH || '';
   const enterDate = process.env.ENTER_DATE || '';
-  const birth = Number(process.env.TRAINEE_BIRTH || '');
+  const className = process.env.CLASS_NAME || '';
+  const groupName = process.env.GROUP_NAME || '';
+  const unitName = process.env.UNIT_NAME || '';
+
+  const soldier = new thecamp.Soldier(
+    name,
+    birth,
+    enterDate,
+    thecamp.SoldierClassName[className],
+    thecamp.SoldierGroupName[groupName],
+    thecamp.SoldierUnitName[unitName],
+    thecamp.SoldierRelationship.FRIEND,
+  );
 
   const cookies = await thecamp.login(id, password);
-  const [group] = await thecamp.fetchGroups(cookies, unitName, enterDate);
+  await thecamp.addSoldier(cookies, soldier);
+  const [trainee] = await thecamp.fetchSoldiers(cookies, soldier);
 
-  const trainee = {
-    birth,
-    traineeName,
-    unitCode: group.unitCode,
-    groupId: group.groupId,
-    relationship: thecamp.Relationship.FRIEND,
-  };
-
-  const message = {
-    title: 'Test title',
-    content: 'Test content',
-  };
+  const message = new thecamp.Message('Title', 'Content', trainee.getTraineeMgrSeq());
 
   await thecamp.sendMessage(cookies, trainee, message);
 })();
