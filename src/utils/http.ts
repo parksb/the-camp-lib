bddaebf456f330c6b30ce93b119d9b33ad404dc4
ybbarng <ybbarng@gmail.com>
2021-06-07 10:37:35 +0900

    HTTP 요청 라이브러리로 request 대신에 axios를 사용하도록 변경

---
+++ src/utils/http.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export interface RequestOption {
+  readonly method: "GET" | "POST" | "PUT";
+  readonly url: string;
+  readonly headers?: any;
+  readonly data?: any;
+  readonly params?: any;
+  readonly json?: boolean;
+}
+
+const removeUndefined = (object?: Object): Record<string, string> => {
+  const result: Record<string, string> = {};
+  if (object) {
+    Object.keys(object).forEach((key) => {
+      if (object[key] !== undefined) {
+        result[key] = object[key];
+      }
+    });
+  }
+  return result;
+};
+
+export const request = async (requestOption: RequestOption) => {
+  const options = {
+    ...requestOption,
+  };
+  if (options.method === "POST") {
+    const headers = {
+      ...options.headers,
+    };
+    if (requestOption.json) {
+      headers["Content-Type"] = "application/json";
+    } else {
+      headers["Content-Type"] = "application/x-www-form-urlencoded";
+      options.data = new URLSearchParams(removeUndefined(requestOption.data));
+    }
+    options.headers = headers;
+  }
+  return await axios(options);
+};
--- test/unit/services/login.test.ts
+++ test/unit/services/login.test.ts
@@ -1,17 +1,15 @@
 import { expect } from 'chai';
-import requestPromise from 'request-promise';
 
-import { buildRequestUrl } from '../../../src/utils';
+import { buildRequestUrl, request, RequestOption } from '../../../src/utils';
 
-const options = (userId?: string, userPwd?: string) => {
+const options = (userId?: string, userPwd?: string): RequestOption => {
   return {
     url: buildRequestUrl('login/loginA.do'),
     method: 'POST',
-    json: true,
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    form: {
+    data: {
       userId,
       userPwd,
       state: 'email-login',
@@ -23,22 +21,25 @@ const options = (userId?: string, userPwd?: string) => {
 describe('login', () => {
   context('존재하지 않는 계정으로 로그인하는 경우', () => {
     it('resultCd 9000 응답이 온다', async () => {
-      const response = await requestPromise(options('user@email.com', 'password'));
-      expect(response.resultCd).to.be.equal('9000');
+      const response = await request(options('user@email.com', 'password'));
+      const data = await response.data;
+      expect(data.resultCd).to.be.equal('9000');
     });
   });
 
   context('이메일을 입력하지 않는 경우', () => {
     it('resultCd 9000 응답이 온다', async () => {
-      const response = await requestPromise(options(undefined, 'password'));
-      expect(response.resultCd).to.be.equal('9006');
+      const response = await request(options(undefined, 'password'));
+      const data = await response.data;
+      expect(data.resultCd).to.be.equal('9006');
     });
   });
 
   context('비밀번호를 입력하지 않는 경우', () => {
     it('resultCd 9000 응답이 온다', async () => {
-      const response = await requestPromise(options('user@email.com'));
-      expect(response.resultCd).to.be.equal('9000');
+      const response = await request(options('user@email.com'));
+      const data = await response.data;
+      expect(data.resultCd).to.be.equal('9000');
     });
   });
 });
